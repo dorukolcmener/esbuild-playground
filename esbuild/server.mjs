68003c0d5c65c3f3ebe617325be88ad0a15e7ab9
unknown <facebondgata@outlook.com>
2023-08-26 21:06:40 +0300

    server log update

--- esbuild/server.mjs
+++ esbuild/server.mjs
@@ -3,4 +3,4 @@ import express from "express";
 const app = express();
 
 app.use(express.static("bundle"));
-app.listen(3000);
+app.listen(3000, () => console.log(`Listening on http://localhost:3000`));
